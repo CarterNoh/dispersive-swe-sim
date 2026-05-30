5e60a7961b8da63390b24180154043258f9af311
Carter Noh <carter.noh08@gmail.com>
2026-05-30 09:42:44 -0600

    adding comments/notes to cell_test

--- cell_test.py
+++ cell_test.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 cellsize = np.array([0.1, 0.15, 0.25, 0.5, 0.75, 1])
-d0 = np.array([1.696, 3.965, 11.252, 45.60, 102.6, 182.5]) # max stable depth for grid = 512 with disturbance of 0.1
-d1 = np.array([1.712, 3.972, 11.260, 45.61, 102.8, 182.9]) # max stable depth for grid = 256 with disturbance of 0.1
+d0 = np.array([1.696, 3.965, 11.252, 45.60, 102.6, 182.5]) # max stable depth for grid = 512 with disturbance of 0.1, timestep 1/60
+d1 = np.array([1.712, 3.972, 11.260, 45.61, 102.8, 182.9]) # max stable depth for grid = 256 with disturbance of 0.1, timestep 1/60
 d_avg = np.mean((d0, d1), axis=0)
 A = np.vstack((cellsize**2, cellsize, np.ones(cellsize.shape))).T
 [b2, b1, b0] = np.linalg.pinv(A) @ d_avg
@@ -14,12 +14,15 @@ plt.plot(cellsize, d1)
 plt.plot(cellsize, d_est)
 plt.legend(("512", "256", "Estimate"))
 plt.show()
+# Result is highly quadratic, a very good fit!
 
 def solve_quadratic(d):
-    a = 182.80027907467993
-    b = 0.045464332332812774
-    c = -0.14717654147795045
+    a = b2 # 182.80027907467993 
+    b = b1 # 0.045464332332812774
+    c = b0 #-0.14717654147795045
     return (-b + np.sqrt(b**2 - 4*a*(c-d))) / (2 * a)
 
 print(solve_quadratic(1.5))
+
+# Note: stability also seems to be somewhat dependant on the timestep, but I didn't have time to do more testing. 
 
